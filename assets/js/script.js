7b3f116d34557f68e8a854191c11d20f03e1560a
heatherzen <heather.zen@gmail.com>
2020-06-24 19:50:45 -0700

    commiting one last time to try and solve problems with git

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,11 @@ var createTaskHandler = function() {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    console.log(taskTypeInput);
     
-
    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
+    
 
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
